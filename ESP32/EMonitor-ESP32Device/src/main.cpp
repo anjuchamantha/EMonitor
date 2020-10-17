7ea57215853a1b778770ba661dd59778c253e70d
Anju Chamantha <chamantha97anju@gmail.com>
2020-10-18 00:25:47 +0530

    [ESP32] Mean, Standard Deviation calculate and send mean

--- ESP32/EMonitor-ESP32Device/src/main.cpp
+++ ESP32/EMonitor-ESP32Device/src/main.cpp
@@ -6,7 +6,6 @@
 #include "wifi_con.h"
 #include "config.h"
 
-
 void setup()
 {
   Serial.begin(115200);
@@ -16,13 +15,66 @@ void setup()
 }
 
 int msg = 0;
+
+double calculate_mean(double arr[], int len)
+{
+  double tot = 0;
+  for (int i = 0; i < len; i++)
+  {
+    tot += arr[i];
+  }
+  return round((tot / len) * 100) / 100.00;
+}
+
+double calculate_sd(double arr[], int len, double mean)
+{
+  double variance = 0;
+  for (int i = 0; i < len; i++)
+  {
+    variance += pow((arr[i] - mean), 2);
+  }
+  return round(sqrt(variance) * 100) / 100.00;
+}
+
 void loop()
 {
   // read_dht();
-  double temperature = round(readTemperature() * 100) / 100.0;
-  double humidity = round(readHumidity() * 100) / 100.0;
-  double pressure = round(readPressure() * 100) / 100.0;
-  double light = round(readLightIntensity() * 100) / 100.0;
+  double temperature = 0;
+  double humidity = 0;
+  double pressure = 0;
+  double light = 0;
+
+  int x = 0;
+  //Get 10 sensor values in 0.5s intervals
+  //Calculate median & s.d for values in 5s intervals
+  int rounds = 10;
+  int round_time = 500;
+
+  //get values and keep in the arrays
+  double t_[rounds];
+  double h_[rounds];
+  double p_[rounds];
+  double l_[rounds];
+
+  while (x < rounds)
+  {
+    t_[x] = round(readTemperature() * 100) / 100.00;
+    h_[x] = round(readHumidity() * 100) / 100.00;
+    p_[x] = round(readPressure() * 100) / 100.00;
+    l_[x] = round(readLightIntensity() * 100) / 100.00;
+    delay(round_time);
+    Serial.println();
+    x++;
+  }
+  temperature = calculate_mean(t_, rounds);
+  humidity = calculate_mean(h_, rounds);
+  pressure = calculate_mean(p_, rounds);
+  light = calculate_mean(l_, rounds);
+
+  double temperature_sd = calculate_sd(t_, rounds, temperature);
+  double humidity_sd = calculate_sd(h_, rounds, humidity);
+  double pressure_sd = calculate_sd(p_, rounds, pressure);
+  double light_sd = calculate_sd(l_, rounds, light);
 
   char xmlchar[1500];
   char identifier[] = "MSG00001";
