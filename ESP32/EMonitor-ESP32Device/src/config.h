a71c2ff174ef6737d23a02c090b34d2f3b1df455
Anju Chamantha <chamantha97anju@gmail.com>
2020-11-08 22:18:31 +0530

    -

---
+++ ESP32/EMonitor-ESP32Device/src/config.h
@@ -0,0 +1,16 @@
+#ifndef SENSITIVE_CONFIG_H
+#define SENSITIVE_CONFIG_H
+
+extern const char *SSID;
+extern const char *PW;
+
+extern const char *serverBase;
+
+#define emailSenderAccount "SENDER_EMAIL"
+#define emailSenderPassword "SENDER_EMAIL_PW"
+#define emailRecipient "RECIPIENT_EMAIL"
+#define smtpServer "smtp.gmail.com"
+#define smtpServerPort 465
+#define emailSubject "EMonitor Device Alert!"
+
+#endif
